f1e9f138f93b7e1ceffe714455a0590acdb2863c
cziko <niznik.pawel@gmail.com>
2009-09-11 16:34:56 +0200

    small changes

--- lib/sys/mem.rb
+++ lib/sys/mem.rb
@@ -1,8 +1,8 @@
 module Sys 
-  mem_file = "/proc/meminfo"
+  $mem_file = "/proc/meminfo"
   $mem_hash = {}
   
-  IO.foreach(mem_file){ |line|
+  IO.foreach($mem_file){ |line|
     line.strip!
     next if line.empty?
 
@@ -21,6 +21,7 @@ module Sys
    
   class MEM
     def self.data_hash
+      self.reload_file
       $mem_hash
     end
 
@@ -30,9 +31,35 @@ module Sys
     (class << self; self; end).class_eval do
       $mem_hash.each_pair do |key, value|
         define_method key.to_sym do
-          value
+          self.reload_file
+          return $mem_hash[key.to_sym]
         end 
       end 
     end 
+
+    #def self.memtotal
+    #end
+    #def self.memfree
+    #end
+
+    def self.reload_file
+      lines = IO.readlines($mem_file)
+      lines.each{ |line|
+        line.strip!
+        next if line.empty?
+
+        key, val = line.split(":")
+        key.strip!
+        key.gsub!(/\s+/,"_")
+        key.downcase!
+        val.strip! if val 
+
+        # Converting strings to bytes
+        match_data_val = /\d*/.match val
+        value = match_data_val[0].to_i * 1024
+        $mem_hash[key.to_sym] = value
+      }
+    end
+
   end
 end
